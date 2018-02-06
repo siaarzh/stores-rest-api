c6f08c8de5afcb9c227f088999b5cf9b35cfec59
siaarzh <siaarzh@gmail.com>
2018-02-06 16:44:10 +0600

    start of section 05-070

--- fill_users_table.py
+++ fill_users_table.py
@@ -13,18 +13,23 @@ cursor = connection.cursor()
 
 delete_table = 'DROP TABLE IF EXISTS users'
 cursor.execute(delete_table)
+
+delete_table = 'DROP TABLE IF EXISTS items'
+cursor.execute(delete_table)
+
 print("dropped table")
 
-create_table = "CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, username VARCHAR (50) UNIQUE, password VARCHAR (8) NOT NULL)"
+# Create users
+create_table = "CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY," \
+			   " username VARCHAR (50) UNIQUE," \
+			   " password VARCHAR (8) NOT NULL)"
 cursor.execute(create_table)
-print("created table")
-
+print("created table users")
 
 user = ('serzhan', 'password')
 insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
 cursor.execute(insert_query, user)
 print("inserted {} into table".format(user[1]))
-
 
 users = [
 	('rolf', 'asdf'),
@@ -42,6 +47,20 @@ row = cursor.fetchone()
 while row is not None:
 	print("\nFetched : \n{}\n".format(row))
 	row = cursor.fetchone()
+
+# Create items
+create_table = "CREATE TABLE IF NOT EXISTS items (name VARCHAR (50) PRIMARY KEY," \
+			   " price REAL)"
+cursor.execute(create_table)
+print("created table items")
+
+item = ('table', 12.99)
+insert_query = "INSERT INTO items (name, price) VALUES (%s, %s)"
+cursor.execute(insert_query, item)
+print("inserted {} into table".format(item[0]))
+
+
+
 
 connection.commit() # saves data
 print('commit achieved')
---
+++ item.py
@@ -0,0 +1,90 @@
+import psycopg2
+from config import config
+
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+
+class Item(Resource):
+
+
+	parser = reqparse.RequestParser()
+	parser.add_argument(
+		'price',
+		type=float,
+		required=True,
+		help='This field cannot be left blank.'
+	)
+
+	@classmethod
+	def db_query(cls, query, query_args): # not using self in the method, but using class name
+
+		connection = None
+		try:
+			params = config()
+			connection = psycopg2.connect(**params)
+			cursor = connection.cursor()
+			cursor.execute(query, query_args) # make sure query_args is a tuple, e.g.: (item,)
+			row = cursor.fetchone()
+
+			if row:
+				return row
+			else:
+				result = None
+			# close the communication with the PostgreSQL
+			cursor.close()
+			return result
+
+		except (Exception, psycopg2.DatabaseError) as error:
+			print(error)
+
+		finally:
+			if connection is not None:
+				connection.close()
+				# print('Database connection closed.')
+
+	def get(self, name):
+		query = "SELECT * FROM items WHERE name=%s"
+		data = self.db_query(query, (name,))
+		if data:
+			return {'item': {'name': data[0], 'price': data[1]}}
+		return {'message': "No item by the name '{}' in table 'items'".format(str(name))}, 404
+
+	# add a dict / status code for non-existent
+	@jwt_required()
+	def post(self, name):
+		# load data only after we check for existing entry
+		if next(filter(lambda x: x['name'] == name, items), None) is not None:
+			return {'message': "An item with the name '{}' already exists".format(name)}, 400
+
+		data = Item.parser.parse_args()
+
+		item = {'name': name, 'price': data['price']}
+		items.append(item)
+		return item, 201  # feedback, status code for CREATED
+
+	@jwt_required()
+	def delete(self, name):
+		global items
+		if next(filter(lambda x: x['name'] == name, items), None) is None:
+			return {'message': "An item with the name '{}' does not exist".format(name)}, 400
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': "Item '{}' deleted.".format(name)}
+
+	@jwt_required()
+	def put(self, name):
+
+		data = Item.parser.parse_args()
+
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			item.update(data)
+		return item
+
+
+class ItemList(Resource):
+	def get(self):
+		return {'items': items}
